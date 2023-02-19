db5d11b7e7ebe8c6722ada179c8fb9d2577ed612
AL <skininja673@gmail.com>
2023-02-19 01:04:16 -0500

    form functionality progress1

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/ContactForm.css';
-import { MyInfo, FormDisclaimer } from '../helpers/Data';
+import { MyInfo } from '../helpers/Data';
+import Form from '../components/Form';
 
 const ContactForm = () => {
     const { mobile, email } = MyInfo;
@@ -31,40 +32,8 @@ const ContactForm = () => {
                 </div>
 
                 {/* <!-- Forms --> */}
-
                 <div className='formBx'>
-                    <form name='contact' method='POST' data-netlify='true'>
-                        <input type='hidden' name='form-name' value='contact' />
-
-                        <input
-                            type='text'
-                            name='name'
-                            placeholder='Name: '
-                            className='styleMe'
-                        />
-                        <input
-                            type='email'
-                            placeholder='Email : '
-                            name='email'
-                            className='styleMe'
-                        />
-                        <textarea
-                            name='message'
-                            rows='6'
-                            id=''
-                            placeholder={`Message: ${FormDisclaimer}`}
-                            className='styleMe'
-                        ></textarea>
-                        <button
-                            type='submit'
-                            // value='Send Message'
-                            name=''
-                            className='btn'
-                            id='submitBtn'
-                        >
-                            send
-                        </button>
-                    </form>
+                    <Form />
                 </div>
             </div>
         </section>
