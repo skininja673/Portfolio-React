9a2a3d1e830feece703ad69beb9afa6212874638
AL <skininja673@gmail.com>
2023-02-21 16:43:58 -0500

    form functionality2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,56 +1,56 @@
 import React from 'react';
-// import { FormDisclaimer } from '../helpers/Data';
+import { FormDisclaimer } from '../helpers/Data';
+
+// const Form = () => {
+//     return (
+//         <form name='contact' action='/contact' method='post'>
+//             <input type='hidden' name='form-name' value='contact' />
+//             <input type='text' name='name' placeholder='your name' />
+//             <input type='email' name='email' placeholder='your email' />
+//             <textarea
+//                 name='message'
+//                 placeholder='your message'
+//                 cols='30'
+//                 rows='10'
+//             ></textarea>
+//             <button type='submit'>send</button>
+//         </form>
+//     );
+// };
 
 const Form = () => {
     return (
         <form name='contact' action='/contact' method='post'>
             <input type='hidden' name='form-name' value='contact' />
-            <input type='text' name='name' placeholder='your name' />
-            <input type='email' name='email' placeholder='your email' />
+
+            <input
+                type='text'
+                name='name'
+                placeholder='Name: '
+                className='styleMe'
+            />
+            <input
+                type='email'
+                placeholder='Email : '
+                name='email'
+                className='styleMe'
+            />
             <textarea
                 name='message'
-                placeholder='your message'
-                cols='30'
-                rows='10'
+                rows='6'
+                id=''
+                placeholder={`Message: ${FormDisclaimer}`}
+                className='styleMe'
             ></textarea>
-            <button type='submit'>send</button>
+            <input
+                type='submit'
+                value='Send Message'
+                name=''
+                className='btn'
+                id='submitBtn'
+            />
         </form>
     );
 };
-
-// const Form = () => {
-//     return (
-//         <form name='contact' method='post' netlify='true'>
-//             <input type='hidden' name='form-name' value='contact' />
-
-//             <input
-//                 type='text'
-//                 name='name'
-//                 placeholder='Name: '
-//                 className='styleMe'
-//             />
-//             <input
-//                 type='email'
-//                 placeholder='Email : '
-//                 name='email'
-//                 className='styleMe'
-//             />
-//             <textarea
-//                 name='message'
-//                 rows='6'
-//                 id=''
-//                 placeholder={`Message: ${FormDisclaimer}`}
-//                 className='styleMe'
-//             ></textarea>
-//             <input
-//                 type='submit'
-//                 value='Send Message'
-//                 name=''
-//                 className='btn'
-//                 id='submitBtn'
-//             />
-//         </form>
-//     );
-// };
 
 export default Form;
