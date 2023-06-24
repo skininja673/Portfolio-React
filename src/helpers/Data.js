6562cd2973c66ac514f035398562df2d19d57fdb
AL <skininja673@gmail.com>
2023-06-24 00:55:15 -0400

    fixed netlify issue

--- src/helpers/Data.js
+++ src/helpers/Data.js
@@ -1,9 +1,9 @@
 import Proj1 from '../assets/proj1.jpg';
 import Proj2 from '../assets/proj2.jpg';
-import Proj3 from '../assets/proj3.jpg';
-import Proj4 from '../assets/proj4.jpg';
-import Proj5 from '../assets/proj5.jpg';
-import Proj6 from '../assets/proj6.jpg';
+// import Proj3 from '../assets/proj3.jpg';
+// import Proj4 from '../assets/proj4.jpg';
+// import Proj5 from '../assets/proj5.jpg';
+// import Proj6 from '../assets/proj6.jpg';
 import Proj7 from '../assets/proj7.jpg';
 import junoClg from '../assets/junoClg.jpg';
 import yorkUni from '../assets/yorkUni.jpg';
